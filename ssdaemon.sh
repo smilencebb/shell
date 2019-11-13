a6c28aca2c453e9d11bf47abadedcf05c0878d89
smilencebb <admin@8ips.com>
2019-11-13 20:54:29 +0800

    Update ssdaemon.sh

--- ssdaemon.sh
+++ ssdaemon.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 while :
 do
-	for i in `netstat -tlnp |grep -E "873|1099" |awk '$2 >125 {print $7}' |cut -d"/" -f1 |awk '!a[$0]++'`
+	for i in `netstat -tlnp |grep -E "873|1099" |awk '$2 >128 {print $7}' |cut -d"/" -f1 |awk '!a[$0]++'`
 	do
 		for j in `docker inspect -f "{{.Id}} {{.State.Pid}}"  $(docker ps -q)|grep $i |awk '{print $1}'`
 		do 
